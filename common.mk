4eedab70ea40de48b9cc41c4d3efa84173b30fe3
LuK1337 <priv.luk@gmail.com>
2019-09-09 20:58:33 +0200

    sdm845-common: Update blobs from OxygenOS 10.3.0
    * Also pin P aptX blobs (they'll be updated in next patch)
    
    Change-Id: I7d90dad4df10770d20818e683f2def428737af02

--- common.mk
+++ common.mk
@@ -97,6 +97,7 @@ PRODUCT_PACKAGES += \
 # Display
 PRODUCT_PACKAGES += \
     libdisplayconfig \
+    libqdMetaData \
     libqdMetaData.system \
     libvulkan \
     vendor.display.config@1.0
@@ -130,6 +131,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.nfc@1.0:64 \
     android.hardware.nfc@1.1:64 \
+    android.hardware.nfc@1.2:64 \
     android.hardware.secure_element@1.0:64 \
     com.android.nfc_extras \
     Tag \
